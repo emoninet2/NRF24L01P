752ba6837b399f1df04c5b4f6674ff0a12db3978
Emon <emon.1992@live.com>
2016-12-12 22:54:18 +0000

    error fix

--- NRF24L01p.cpp
+++ NRF24L01p.cpp
@@ -34,41 +34,46 @@ int NRF24L01p::fifo_init(fifo_t *f, Payload_t  *pld, unsigned int size){
 }
 
 int NRF24L01p::fifo_read(fifo_t *f, Payload_t  *pld){
+    int retval = 0;
     if(f->tail != f->head){
         *(pld) = (f->payload[f->tail]);
         f->tail++;
         if(f->tail == f->size){
             f->tail = 0;
         }
-        return 0;
+        retval =  0;
     }
     else {
-        return -1;
+        retval =  -1;
     }
+    
+    return retval;
 }
 int NRF24L01p::fifo_peekNext(fifo_t *f, Payload_t  *pld){
+    int retval;
     if(f->tail != f->head){
         *(pld) = (f->payload[f->tail]);
-        return 0;
+        retval =  0;
     }
     else {
-        return -1;
+        retval =  -1;
     }
-
+    return retval;
 }
 int NRF24L01p::fifo_write(fifo_t *f, Payload_t  *pld){
-    if((f->head + 1 == f->tail) || (f->head  == f->size - 1)&&(f->tail == 0)    ){
-        return -1;
+    int retval;
+    if((f->head + 1 == f->tail) || (f->head+1  == f->size )&&(f->tail == 0)    ){
+        retval =  -1;
     }
     else{
-        (f->payload[f->head]) = (*pld);
+        f->payload[f->head] = *(pld);
         f->head++;
         if(f->head == f->size){
             f->head = 0;
         }
-        return 0;
+        retval =  0;
     }
-
+    return retval;
 }
 
 
@@ -84,7 +89,6 @@ void NRF24L01p::initialize(){
     default_config();
     
 
-    
     fifo_init(&TxFifo, TxFifoBuffer, NRF24L01P_FIFO_SIZE);
     fifo_init(&RxFifo, RxFifoBuffer, NRF24L01P_FIFO_SIZE);
     
@@ -94,105 +98,105 @@ void NRF24L01p::initialize(){
 
 
 int NRF24L01p::startup(){
-	Port_CE_pin(0);
-	Port_CSN_pin(0);
-	
-	Port_Delay_ms(_NRF24L01P_TIMING_PowerOnReset_ms);
-	
-	RadioMode(MODE_POWER_DOWN);
-	RadioMode(MODE_RX);
-	
-        clear_data_ready_flag();
-	flush_rx();
-	flush_tx();
-	
-	uint8_t status_rst_val = 0x70;//reset status
-	write_register(_NRF24L01P_REG_STATUS, &status_rst_val,1);
-	uint8_t config_rst_val = 0x0b;//reset config
-	write_register(_NRF24L01P_REG_CONFIG, &config_rst_val,1);
-        
-        disable_auto_ack_all_pipes();
-	disable_dynamic_payload_all_pipe();/////////ALSO CREEATE FOR DISABLE AUTO ACK FOR ALL PIPE
-	return 0;
+    Port_CE_pin(0);
+    Port_CSN_pin(0);
+
+    Port_Delay_ms(_NRF24L01P_TIMING_PowerOnReset_ms);
+
+    RadioMode(MODE_POWER_DOWN);
+    RadioMode(MODE_RX);
+
+    clear_data_ready_flag();
+    flush_rx();
+    flush_tx();
+
+    uint8_t status_rst_val = 0x70;//reset status
+    write_register(_NRF24L01P_REG_STATUS, &status_rst_val,1);
+    uint8_t config_rst_val = 0x0b;//reset config
+    write_register(_NRF24L01P_REG_CONFIG, &config_rst_val,1);
+
+    disable_auto_ack_all_pipes();
+    disable_dynamic_payload_all_pipe();/////////ALSO CREEATE FOR DISABLE AUTO ACK FOR ALL PIPE
+    return 0;
 }
 
 int NRF24L01p::default_config(){
 
-	enable_dynamic_payload();
-	enable_payload_with_ack();
+    enable_dynamic_payload();
+    enable_payload_with_ack();
 
-	enable_auto_ack(PIPE_P0);
-	enable_auto_ack(PIPE_P1);
-	enable_auto_ack(PIPE_P2);
-	enable_auto_ack(PIPE_P3);
-	enable_auto_ack(PIPE_P4);
-	enable_auto_ack(PIPE_P5);
+    enable_auto_ack(PIPE_P0);
+    enable_auto_ack(PIPE_P1);
+    enable_auto_ack(PIPE_P2);
+    enable_auto_ack(PIPE_P3);
+    enable_auto_ack(PIPE_P4);
+    enable_auto_ack(PIPE_P5);
 
-	enable_dynamic_payload_pipe(PIPE_P0);
-	enable_dynamic_payload_pipe(PIPE_P1);
-	enable_dynamic_payload_pipe(PIPE_P2);
-	enable_dynamic_payload_pipe(PIPE_P3);
-	enable_dynamic_payload_pipe(PIPE_P4);
-	enable_dynamic_payload_pipe(PIPE_P5);
+    enable_dynamic_payload_pipe(PIPE_P0);
+    enable_dynamic_payload_pipe(PIPE_P1);
+    enable_dynamic_payload_pipe(PIPE_P2);
+    enable_dynamic_payload_pipe(PIPE_P3);
+    enable_dynamic_payload_pipe(PIPE_P4);
+    enable_dynamic_payload_pipe(PIPE_P5);
 
-	enable_rx_on_pipe(PIPE_P0);
-	enable_rx_on_pipe(PIPE_P1);
-	enable_rx_on_pipe(PIPE_P2);
-	enable_rx_on_pipe(PIPE_P3);
-	enable_rx_on_pipe(PIPE_P4);
-	enable_rx_on_pipe(PIPE_P5);
+    enable_rx_on_pipe(PIPE_P0);
+    enable_rx_on_pipe(PIPE_P1);
+    enable_rx_on_pipe(PIPE_P2);
+    enable_rx_on_pipe(PIPE_P3);
+    enable_rx_on_pipe(PIPE_P4);
+    enable_rx_on_pipe(PIPE_P5);
 
-	set_auto_retransmission_count(15);
-	set_auto_retransmission_delay(15);
-        
-	set_DataRate(RF_SETUP_RF_DR_250KBPS);
-	set_DataRate(RF_SETUP_RF_DR_2MBPS);
-	return 0;
+    set_auto_retransmission_count(15);
+    set_auto_retransmission_delay(15);
+
+    set_DataRate(RF_SETUP_RF_DR_250KBPS);
+    set_DataRate(RF_SETUP_RF_DR_2MBPS);
+    return 0;
 }
 
 void NRF24L01p::RadioMode(NRF24L01p::StateType mode){
-	switch(mode){
-		case MODE_POWER_DOWN: {
-                    power_down();
+    switch(mode){
+        case MODE_POWER_DOWN: {
+            power_down();
+            Port_CE_pin(0);
+            RadioState = MODE_POWER_DOWN;
+            break;
+        }
+        case MODE_STANDBY: {
+            if(RadioState == MODE_POWER_DOWN){
+                    power_up();
+                    Port_Delay_us(_NRF24L01P_TIMING_Tpd2stby_us);
+            }
+            else{
                     Port_CE_pin(0);
-                    RadioState = MODE_POWER_DOWN;
-                    break;
-		}
-		case MODE_STANDBY: {
-                    if(RadioState == MODE_POWER_DOWN){
-                            power_up();
-                            Port_Delay_us(_NRF24L01P_TIMING_Tpd2stby_us);
-                    }
-                    else{
-                            Port_CE_pin(0);
-                    }
-                    RadioState = MODE_STANDBY;
-                    break;
-		}
-		case MODE_RX: {
-                    if(RadioState != MODE_RX){
-                        Port_CE_pin(0);
-                        rx_mode();
-                        Port_CE_pin(1);
-                        Port_Delay_us(_NRF24L01P_TIMING_Tstby2a_us);
-                        RadioState = MODE_RX;
-                    }
+            }
+            RadioState = MODE_STANDBY;
+            break;
+        }
+        case MODE_RX: {
+            if(RadioState != MODE_RX){
+                Port_CE_pin(0);
+                rx_mode();
+                Port_CE_pin(1);
+                Port_Delay_us(_NRF24L01P_TIMING_Tstby2a_us);
+                RadioState = MODE_RX;
+            }
 
-                    break;
-		}
-		case MODE_TX: {
-                    if(RadioState != MODE_TX){
-                        Port_CE_pin(0);
-                        tx_mode();
-                        Port_CE_pin(1);
-                        Port_Delay_us(_NRF24L01P_TIMING_Tstby2a_us);
-                        RadioState = MODE_TX;
-                    }
-                    
+            break;
+        }
+        case MODE_TX: {
+            if(RadioState != MODE_TX){
+                Port_CE_pin(0);
+                tx_mode();
+                Port_CE_pin(1);
+                Port_Delay_us(_NRF24L01P_TIMING_Tstby2a_us);
+                RadioState = MODE_TX;
+            }
 
-                    break;
-		}		
-	}
+
+            break;
+        }		
+    }
 }
 
 
@@ -208,13 +212,13 @@ bool NRF24L01p::writable(){
 
 
 int NRF24L01p::write_payload_to_send(uint8_t *data, int datalen){
-	if ( datalen <= 0 ) return 0;
-	if ( datalen > _NRF24L01P_FIFO_SIZE ) datalen = _NRF24L01P_FIFO_SIZE;
+    if ( datalen <= 0 ) return 0;
+    if ( datalen > _NRF24L01P_FIFO_SIZE ) datalen = _NRF24L01P_FIFO_SIZE;
 
-	if(get_tx_fifo_full_flag()) return -1;
-	//while(_nrf24l01p_get_tx_fifo_full_flag());
-	write_tx_payload(data,datalen);
-	return 0;
+    if(get_tx_fifo_full_flag()) return -1;
+    //while(_nrf24l01p_get_tx_fifo_full_flag());
+    write_tx_payload(data,datalen);
+    return 0;
 
 }
 
@@ -343,13 +347,16 @@ void NRF24L01p::PRX(){
             if(fifo_write(&RxFifo, &payload) <= 0)break;
             if(!readable()) break;
         }
-        
+        clear_data_ready_flag();
         RadioMode(originalState);
     }
 }
 void NRF24L01p::PTX(){
     Payload_t payload;
-    if(fifo_read(&TxFifo, &payload) == -1) return;
+    int r = fifo_read(&TxFifo, &payload);
+    if(r == -1){
+        return;
+    }
     else{
         //enable_payload_with_ack();
         write_payload_to_send_to_address_ack(payload.TxAddr, payload.data, sizeof(payload.data));
@@ -357,6 +364,7 @@ void NRF24L01p::PTX(){
         StateType  originalState = RadioState;
 
         if(writable() && !get_fifo_flag_rx_full())   {
+            clear_data_sent_flag();
             while(1){
                 RadioMode(MODE_TX);   
                 RadioMode(MODE_STANDBY);   
@@ -380,7 +388,7 @@ void NRF24L01p::PTX(){
         //if got ack packet, just flush it
         if(get_data_ready_flag()){
             //do what needs to be done with the ACK payload here
-            //flush_rx();//if you want to flush RX, (use only if the PTX started when RX was empty)
+            flush_rx();//if you want to flush RX, (use only if the PTX started when RX was empty)
         }
 
         //restore original machine state
