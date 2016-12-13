7e1ed34053090b820398a68bbc7be5d659249133
Emon <emon.1992@live.com>
2016-12-13 11:03:23 +0000

    added port for mbed

---
+++ port/MBED/NRF24L01pPortDriverConfig_MBED.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   NRF24L01pPortDriverConfig_MBED.h
+ * Author: emon1
+ *
+ * Created on December 13, 2016, 2:58 PM
+ */
+
+#ifndef NRF24L01PPORTDRIVERCONFIG_MBED_H
+#define NRF24L01PPORTDRIVERCONFIG_MBED_H
+
+
+
+#endif /* NRF24L01PPORTDRIVERCONFIG_MBED_H */
+
