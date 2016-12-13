8289de4626b3a9b92ad18be532a9ac984862f827
Emon <emon.1992@live.com>
2016-12-13 13:35:14 +0000

    updated network class

--- NRF24L01pNetwork.cpp
+++ NRF24L01pNetwork.cpp
@@ -13,6 +13,13 @@
 
 #include "NRF24L01pNetwork.h"
 
+#include <inttypes.h>
+#include <stdint.h>
+#include <limits>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+
 NRF24L01pNetwork::NRF24L01pNetwork() {
 }
 
@@ -22,3 +29,74 @@ NRF24L01pNetwork::NRF24L01pNetwork(const NRF24L01pNetwork& orig) {
 NRF24L01pNetwork::~NRF24L01pNetwork() {
 }
 
+
+
+void NRF24L01pNetwork::init_network(){
+    set_RX_pipe_address(PIPE_P1, ((uint64_t)ownNetworkId<<24) +( (uint64_t)(ownNodeId)<<8) + (uint64_t)0xC2);
+}
+
+
+
+void NRF24L01pNetwork::processPacket(Payload_t *payload){
+    network_payload_t *network_pld = (network_payload_t*) payload->data;
+    //printf("your message was : %s\r\n", payload->data);
+    //printf("your address was : %llx\r\n", payload.TXaddress);
+    //printf("your pipe was : %d\r\n", payload->RXpipe);
+    printf("\r\n");
+    printf("fromAddr : %x\r\n", network_pld->fromNodeId);
+    printf("toAddr : %x\r\n", network_pld->toNodeId);
+    printf("pid : %x\r\n", network_pld->pid);
+    printf("payload data : %s\r\n", network_pld->payload);
+    
+
+    if(ownIdMatched(payload) ){
+        printf("address matched\r\n");
+    }
+    else{
+        printf("bouncing packet\r\n");
+    }
+}
+
+
+bool NRF24L01pNetwork::ownIdMatched(Payload_t *payload){
+    network_payload_t *network_pld = (network_payload_t*) payload->data;
+    if((network_pld->toNodeId) == ownNodeId ){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int NRF24L01pNetwork::sendToNetwork(network_payload_t *Netpayload){
+	//if( isNodeReachable(Netpayload->toNodeId)){
+	//	sendToNodeDirect(Netpayload);
+	//}
+	//else{
+		sendToAllAdjacent(Netpayload);
+	//}
+	return 0;
+}
+
+
+int NRF24L01pNetwork::sendToNodeDirect(network_payload_t *Netpayload){
+
+}
+
+int NRF24L01pNetwork::sendToAllAdjacent(network_payload_t *Netpayload){
+
+    Payload_t payload;
+    int i;
+    memcpy((char*)payload.data, (char*)Netpayload, 32);
+    for(i=0;i<5;i++){
+        payload.TxAddr = ((uint64_t)ownNetworkId<<24) +( (uint64_t)(reachable_Nodes[i])<<8) + (uint64_t)0xC2;
+        fifo_write(&TxFifo, &payload);
+    }
+    return 0;
+}
+
+
+bool NRF24L01pNetwork::isNodeReachable(uint16_t NodeId){
+
+}
--- NRF24L01pNetwork.h
+++ NRF24L01pNetwork.h
@@ -16,11 +16,42 @@
 
 #include "NRF24L01p.h"
 
+#include <inttypes.h>
+#include <stdint.h>
+#include <limits>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+
 class NRF24L01pNetwork : public NRF24L01p{
 public:
+    typedef struct network_payload{
+        uint16_t fromNodeId;
+        uint16_t toNodeId;
+        uint8_t pid;
+        uint8_t packet_info;
+        uint8_t payload[26];
+    }network_payload_t;
+
+    uint16_t reachable_Nodes[5];
+    
+    uint16_t ownNetworkId;
+    uint16_t ownNodeId;
+    
+    
     NRF24L01pNetwork();
     NRF24L01pNetwork(const NRF24L01pNetwork& orig);
     virtual ~NRF24L01pNetwork();
+    
+    void init_network();
+    void processPacket(Payload_t *payload);
+    bool ownIdMatched(Payload_t *payload);
+    
+    int sendToNetwork(network_payload_t *Netpayload);
+    int sendToNodeDirect(network_payload_t *Netpayload);
+    int sendToAllAdjacent(network_payload_t *Netpayload);
+    bool isNodeReachable(uint16_t NodeId);
+    
 private:
 
 };
