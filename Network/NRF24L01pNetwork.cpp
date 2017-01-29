b32bcb84c53d3c53f65ca73f34aacf606797095d
Emon <emon.1992@live.com>
2017-01-29 14:37:04 +0000

    testing if disabling auto ack prevents ack on pipe 0

--- Network/NRF24L01pNetwork.cpp
+++ Network/NRF24L01pNetwork.cpp
@@ -23,15 +23,17 @@ NRF24L01pNetwork::~NRF24L01pNetwork() {
 }
 
 void NRF24L01pNetwork::enableBroadcast(bool sel){
+    disable_auto_ack(PIPE_P0);
     set_RX_pipe_address((pipe_t) 0,NRF24L01P_NETWORK_BROADCAST_ADDR);
 }
 int NRF24L01pNetwork::processBroadcastPacket(Payload_t *payload){
     printf("got broadcast packet\r\n");
 }
 int NRF24L01pNetwork::broadcastPacket(Payload_t *payload){
-    printf("sending broadcast\r\n");
     payload->TxAddr = NRF24L01P_NETWORK_BROADCAST_ADDR;
+    disable_auto_ack(PIPE_P0);
     int ret = TransmitPacket(payload);
+    disable_auto_ack(PIPE_P0);
     return ret;
 }
 int NRF24L01pNetwork::assignToAdjacent(AdjNode_t *node){
