29cc2ea811dc4d5ea2a9666f05e9c7b08e7de1c7
Emon <emon.1992@live.com>
2017-02-04 15:59:33 +0000

    cleaned library

--- NRF24L01p.h
+++ NRF24L01p.h
@@ -23,73 +23,7 @@ public:
     NRF24L01p();
     NRF24L01p(const NRF24L01p& orig);
     virtual ~NRF24L01p();
-    
-    
-    typedef struct{
-        uint8_t data[32];
-        uint64_t TxAddr;
-        pipe_t RxPipe;
-        uint8_t len;
-    }Payload_t;
-    
-    typedef enum {
-           MODE_UNKNOWN = 0,/**< NRF24L01+ unknown mode */
-           MODE_POWER_DOWN,/**< NRF24L01+ Power Down mode */
-           MODE_STANDBY,/**< NRF24L01+ Standby mode */
-           MODE_RX,/**< NRF24L01+ RX mode */
-           MODE_TX,/**< NRF24L01+ TX mode */
-    } StateType;
 
-    typedef struct{
-        Payload_t *payload;
-        unsigned int head;
-        unsigned int tail;
-        unsigned int size;
-    }fifo_t;
-    
-    
-    int fifo_init(fifo_t *f, Payload_t  *pld, unsigned int size);
-    int fifo_read(fifo_t *f, Payload_t  *pld);
-    int fifo_peekNext(fifo_t *f, Payload_t  *pld);
-    int fifo_write(fifo_t *f, Payload_t  *pld);
-
-    
-    StateType RadioState;
-    
-    Payload_t RxFifoBuffer[NRF24L01P_FIFO_SIZE ];
-    Payload_t TxFifoBuffer[NRF24L01P_FIFO_SIZE ];
-    
-    fifo_t TxFifo;
-    fifo_t RxFifo;
-
-    uint64_t TxAddr;
-
-    void initialize();
-    int startup();
-    int default_config();
-    void RadioMode(StateType mode);
-    
-    bool readable();
-    bool writable();
-
-    int write_payload_to_send(uint8_t *data, int datalen);
-    int write_payload_to_send_to_address(pipeAddrType_t address, uint8_t *data, int datalen);
-    int write_payload_to_send_to_address_ack(pipeAddrType_t address, uint8_t *data, int datalen);
-    int write_payload_to_send_to_address_noack(pipeAddrType_t address, uint8_t *data, int datalen);
-    bool readableOnPipe(pipe_t pipe);
-    int read_payload(pipe_t pipe, uint8_t *data, int datalen);
-    int read_payload_dyn(pipe_t pipe, uint8_t *data);
-    void write_ack(pipe_t pipe, uint8_t *data, int datalen);
-
-    int TransmitPacket(Payload_t *payload);
-    int ReceivePacket(Payload_t *payload);
-    
-    
-    void PRX();
-    void PTX();
-    
-    
-    
 private:
 
 };
